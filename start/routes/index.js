b9e4f4db840b86cfd62a6409de0d808314563323
ivanko pavel <ivanko.pavel@gmail.com>
2021-08-26 02:22:37 +0300

    rc2

--- start/routes/index.js
+++ start/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express','extra':{'info':'just extra info','version':'rc1'} });
+  res.render('index', { title: 'Express','extra':{'info':'just extra info','version':'rc2'} });
 });
 
 module.exports = router;
